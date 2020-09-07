f7db60b3e7a5b40da89d296513b32aa473cea73d
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-07 17:26:04 +0100

    Update std.h
    Added THROW and CATCH
    Improved documentation

--- std.h
+++ std.h
@@ -1,11 +1,22 @@
 #pragma once
 
+// Gets the textual representation of the name of a class (without the struct
+// before it)
 #define CLASS_NAME(name) #name
 
-#define IS_SAME(some_type, expression) \
+// Checks if the type from a base class pointer is equal to the type of a
+// derived class pointer
+#define IS_SAME(some_type, expression)                                         \
   (strcmp(expression->typeid_name(), CLASS_NAME(some_type)) == 0)
 
-#define DYNAMIC_CAST(new_type, expression) \
-  (IS_SAME(new_type, expression)) ? \
-    (struct new_type const *)a : \
-    NULL
+// Checks if a base class pointer can be cast to a derived class pointer
+#define DYNAMIC_CAST(new_type, expression)                                     \
+  (IS_SAME(new_type, expression)) ? (struct new_type const *)a : NULL
+
+// Simulates throwing an exception by setting a bool error value
+#define THROW(error_pointer)                                                   \
+  if (error_pointer)                                                           \
+    *error_pointer = true;
+
+// Simulates catching an exception by checking a bool error value
+#define CATCH(error_pointer) (error_pointer && *error_pointer)
