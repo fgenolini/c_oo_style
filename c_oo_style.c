b946b0872fcc1e53f256fc9e5f4a895644561f49
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-07 16:38:44 +0100

    Update c_oo_style.c
    Use updated dynamic_cast simulation.
    Allocate big_animal on the stack instead of on the heap

--- c_oo_style.c
+++ c_oo_style.c
@@ -3,20 +3,21 @@
 
 #include "animal.h"
 #include "big_animal.h"
+#include "std.h"
 
 // Inject object, so that calling function can simulate RAII
 void use_animal(struct animal const *a) {
   printf("%s %.*s\n", a->typeid_name(), (int)a->length(a), a->say(a));
+  struct big_animal const *b_a = DYNAMIC_CAST(big_animal, a);
+  if (!b_a)
+    return;
 
-  // Dynamic cast
-  if (strcmp(a->typeid_name(), CLASS_NAME(big_animal)) == 0) {
-    struct big_animal const *b_a = (struct big_animal const *)a;
-    printf("  repeated %ld times\n", b_a->repeat_count(b_a));
-  }
+  printf("  repeated %ld times\n", b_a->repeat_count(b_a));
 }
 
 // RAII simulated by calling a "use" function
 // between calls of constructor and destructor
+// with animal allocated on the heap
 void animal_raii() {
   // Constructor exception simulated by error parameter
   bool animal_alloc_error = false;
@@ -34,9 +35,11 @@ void animal_raii() {
 }
 
 // Polymorphic call to derived functions from base object pointer
+// with big_animal allocated on the stack
 void big_animal_polymorphic() {
   bool animal_alloc_error = false;
-  struct big_animal *b_a = big_animal_alloc("moo", 2ul, &animal_alloc_error);
+  struct big_animal b_a;
+  big_animal_make(&b_a, "moo", 2ul, &animal_alloc_error);
   if (animal_alloc_error)
     exit(EXIT_FAILURE);
 
@@ -46,15 +49,15 @@ void big_animal_polymorphic() {
   // Since VMTs overlap (fist functions have the same semantics)
   // casting the derived object to the base object will work
   // if only functions are called, and no private data is accessed
-  struct animal *base = (struct animal *)b_a;
+  struct animal *base = (struct animal *)&b_a;
 
   // Call function using the base class pointer,
   // calling the virtual function in the derived class, not the base class
   use_animal(base);
 
-  // Free using the base class pointer,
+  // De-initialise using the base class pointer,
   // calling the virtual destructor for the derived class, not the base class
-  animal_free((struct animal **)&b_a);
+  animal_release(base);
 }
 
 int main() {
