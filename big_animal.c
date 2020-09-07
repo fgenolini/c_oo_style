58b7111ff571572aab2af8ef64bd199d69c02ce0
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-07 03:02:41 +0100

    Update big_animal.c
    Added definition for the repeat_count function.
    Free the big_sound buffer upon release.
    Avoid using base class private data members, and instead use public virtual functions.

--- big_animal.c
+++ big_animal.c
@@ -7,10 +7,17 @@ char const *big_animal_typeid_name() { return CLASS_NAME(big_animal); }
 
 void big_animal_release(struct big_animal *self) {
   // printf("big_animal_release\n");
+  self->length = NULL;
   self->release = NULL;
+  self->repeat_count = NULL;
   self->say = NULL;
-  self->private.repeat_count = 0;
+  self->typeid_name = NULL;
+  if (self->private.big_sound)
+    free(self->private.big_sound);
+
+  self->private.big_sound = NULL;
   self->private.big_sound_length = 0;
+  self->private.repeat_count = 0;
   self->base.release(&self->base);
 }
 
@@ -31,6 +38,10 @@ size_t big_animal_length(struct big_animal const *self) {
   return self->private.big_sound_length;
 }
 
+size_t big_animal_repeat_count(struct big_animal const *self) {
+  return self->private.repeat_count;
+}
+
 void big_animal_make(struct big_animal *self, char const *sound,
                      size_t repeat_count, bool *has_error) {
   // printf("big_animal_make\n");
@@ -40,12 +51,12 @@ void big_animal_make(struct big_animal *self, char const *sound,
 
   self->private.repeat_count = repeat_count;
   size_t l = 1;
-  size_t sound_length = self->base.private.sound_length;
+  size_t sound_length = self->base.length(&self->base);
   if (repeat_count > 0)
     l = 1 + (repeat_count - 1) + repeat_count * sound_length;
 
   self->private.big_sound = (char *)calloc(l, sizeof(char));
-  char const *s = self->base.private.sound;
+  char const *s = self->base.say(&self->base);
   char *c = self->private.big_sound;
   for (size_t i = 0; i < repeat_count; ++i) {
     if (i > 0) {
@@ -60,9 +71,10 @@ void big_animal_make(struct big_animal *self, char const *sound,
   }
 
   self->private.big_sound_length = strlen(self->private.big_sound);
-  self->release = big_animal_release;
-  self->say = big_animal_say;
   self->length = big_animal_length;
+  self->release = big_animal_release;
+  self->repeat_count = big_animal_repeat_count;
+  self->say = big_animal_say;
   self->typeid_name = big_animal_typeid_name;
 }
 
