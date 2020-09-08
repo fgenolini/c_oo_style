203b6b5c0ec6339f28c1a3111db4bf0dad4bf245
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-08 12:19:09 +0100

    Update c_oo_style.c
    Fix warnings from Microsoft Visual C compiler.
    Refactoring for clarity and coherence

--- c_oo_style.c
+++ c_oo_style.c
@@ -5,6 +5,9 @@
 #include "big_animal.h"
 #include "std.h"
 
+#define ANIMAL_NOISE "eek"
+#define BIG_ANIMAL_NOISE "moo"
+
 // Inject object, so that calling function can simulate RAII
 static void use_animal(struct animal const *a) {
   printf("%s %.*s\n", a->typeid_name(), (int)a->length(a), a->say(a));
@@ -12,7 +15,7 @@ static void use_animal(struct animal const *a) {
   if (!b_a)
     return;
 
-  printf("  repeated %ld times\n", b_a->repeat_count(b_a));
+  printf("  repeated %zd times\n", b_a->repeat_count(b_a));
 }
 
 // RAII simulated by calling a "use" function
@@ -20,7 +23,7 @@ static void use_animal(struct animal const *a) {
 // with animal allocated on the heap
 static void raii_heap(bool *has_error) {
   // Constructor called before "use" function
-  struct animal *a = animal_alloc("eek", has_error);
+  struct animal *a = animal_alloc(ANIMAL_NOISE, has_error);
   if (CATCH(has_error))
     return;
 
@@ -34,29 +37,29 @@ static void raii_heap(bool *has_error) {
 // with big_animal allocated on the stack
 static void raii_stack_polymorphic(bool *has_error) {
   struct big_animal b_a;
-  big_animal_make(&b_a, "moo", 2ul, has_error);
+  big_animal_make(&b_a, BIG_ANIMAL_NOISE, 2ul, has_error);
   if (CATCH(has_error))
     return;
 
   // Since VMTs overlap (fist functions have the same semantics)
   // casting the derived object to the base object will work
   // if only functions are called, and no private data is accessed
-  struct animal *base = (struct animal *)&b_a;
+  struct animal *a = (struct animal *)&b_a;
 
   // Call function using the base class pointer,
   // calling the virtual function in the derived class, not the base class
-  use_animal(base);
+  use_animal(a);
 
   // De-initialise using the base class pointer,
   // calling the virtual destructor for the derived class, not the base class
-  animal_release(base);
+  animal_release(a);
 }
 
 // Exceptions simulated by error value
 static bool exception_as_error = false;
 static bool *has_error = &exception_as_error;
 
-int main() {
+int main(void) {
   raii_heap(has_error);
   if (CATCH(has_error))
     exit(EXIT_FAILURE);
@@ -66,5 +69,4 @@ int main() {
     exit(EXIT_FAILURE);
 
   exit(EXIT_SUCCESS);
-  return 0;
 }
